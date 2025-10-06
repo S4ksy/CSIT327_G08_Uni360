05b65a07dfe1f9ea311acd1c54cef78ff3765593
James Andrew Magdales <bonjingmagdales112@gmail.com>
2025-10-07 04:50:27 +0800

    Only accessible kay Login, Signup, Dashboard

--- core/pos/views.py
+++ core/pos/views.py
@@ -3,9 +3,6 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 
-def home(request):
-    return render(request, 'pos/home.html')
-
 def login_view(request):
     if request.method == 'POST':
         username = request.POST['username']
